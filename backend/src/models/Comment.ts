2b5a106845c82a3c36e8eb74742efca6d303ea5c
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-07-25 21:35:20 -0700

    finished api (for now)

--- backend/src/models/Comment.ts
+++ backend/src/models/Comment.ts
@@ -1,14 +1,17 @@
 import { IUser } from "./User";
 import mongoose, { Document, Schema } from "mongoose";
+import { IPost } from "./Post";
 
 export interface IComment extends Document {
   owner: IUser["_id"];
+  post: IPost["_id"];
   text: string;
 }
 
 const CommentSchema: Schema = new Schema(
   {
-    owner: { type: mongoose.Types.ObjectId, required: true },
+    owner: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    post: { type: mongoose.Types.ObjectId, required: true, ref: "Post" },
     text: { type: String, required: true },
   },
   {
