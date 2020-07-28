721f1de992f3e6df8b6bd0b94a5d78113e988c5c
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-07-28 10:23:25 -0700

    fixed /all endpoint

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Container, Header, Icon } from "semantic-ui-react";
+
+interface IComment {
+  owner: string;
+  text: string;
+}
+
+interface IProps {
+  title: string;
+  owner: string;
+  _id: string;
+  image: string;
+  likes: number;
+}
+
+//eslint-disable-nextline
+const Post: React.FC<IProps> = ({ title, owner, _id, image, likes }) => {};
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container text>
+      <Header as="h1" textAlign="center">
+        <Icon name="image" /> Instagram Demo
+      </Header>
+    </Container>
   );
 };
 
